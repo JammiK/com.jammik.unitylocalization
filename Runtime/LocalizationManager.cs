c9998f9bb6af40cec0ee5fffe235ef7ee25c0464
Vadim Solonukhin <jammik94@gmail.com>
2020-06-24 00:33:49 -0400

    add preserve assembly

--- Runtime/LocalizationManager.cs
+++ Runtime/LocalizationManager.cs
@@ -1,8 +1,11 @@
-using System;
 using System.Collections.Generic;
 using System.Data;
 using Jammik.Localization.Interfaces;
 using UnityEngine;
+using UnityEngine.Scripting;
+
+
+[assembly: Preserve]
 
 namespace Jammik.Localization
 {
